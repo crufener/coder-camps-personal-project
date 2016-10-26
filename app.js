18363db65bda858484de67c709132b52a27b84e8
Craig Rufener <crufener@outlook.com>
2016-10-26 00:14:29 -0700

    added main route and view

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 "use strict";
 var express = require("express");
+var path = require("path");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var index_1 = require("./routes/index");
 var app = express();
+app.set('view engine', 'ejs');
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
+app.use('/ngApp', express.static(path.join(__dirname, 'ngApp')));
+app.use('/', index_1.default);
--- app.ts
+++ app.ts
@@ -1,5 +1,24 @@
 import * as express from 'express';
+import * as path from 'path';
+import * as favicon from 'serve-favicon';
+import * as logger from 'morgan';
+import * as cookieParser from 'cookie-parser';
 import * as bodyParser from 'body-parser';
-import * as mongoose from 'mongoose';
+import * as ejs from 'ejs';
+
+import routes from './routes/index';
 
 let app = express();
+
+// view engine setup
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
+app.use('/ngApp', express.static(path.join(__dirname, 'ngApp')));
+
+app.use('/', routes);
